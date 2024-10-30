3f908aac977a5a348c5e53cc4f9ba718b3f9da73
fleasion <etcy@duck.com>
2024-10-30 12:51:09 +0700

    download fix

---
+++ download/download.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+  const hasDownloaded = localStorage.getItem('hasDownloaded');
+
+  if (!hasDownloaded) {
+      const link = document.createElement("a");
+
+      link.href = "fleasion.zip";
+      link.download = "fleasion.zip"; 
+
+      document.body.appendChild(link);
+
+      link.click();
+
+      localStorage.setItem('hasDownloaded', 'true');
+  }
+}
